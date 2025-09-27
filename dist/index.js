443544380a3be811bfacdb409ca8765bff809dff
Your Name <you@example.com>
2025-09-27 20:12:57 +0530

    port add

--- dist/index.js
+++ dist/index.js
@@ -89,6 +89,6 @@ app.delete('/products/:id', (req, res) => {
         });
     });
 });
-app.listen(port, () => {
+app.listen(port,'0.0.0.0', () => {
     console.log(`Server running on port ${port}`);
 });
